565a0fdeeffde7b6ac6e11d311167a2d445418be
bhaskar <bhaskar@thoughtworks.com>
2016-01-09 14:24:36 +0000

    Added Tests for Mappers.|<Bhaskar/Shabbir>

---
+++ FractionalCalculator/FractionalCalculator.Unit.Tests/Mappers/FractionMapperTests.cs
@@ -0,0 +1,48 @@
+﻿using FractionalCalculator.Domain;
+using FractionalCalculator.Mappers;
+using FractionalCalculator.Models;
+using NUnit.Framework;
+
+namespace FractionalCalculator.Unit.Tests.Mappers
+{
+    [TestFixture]
+    public class FractionMapperTests
+    {
+        private FractionModel _model;
+        private Fraction _domain;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _model = new FractionModel
+            {
+                Numerator = 3,
+                Denominator = 7
+            };
+
+            _domain = new Fraction(5, 17);
+        }
+
+        [Test]
+        public void ShouldMapFractionModelToFraction()
+        {
+            var _domain = FractionMapper.Map(_model);
+
+            Assert.IsNotNull(_domain);
+
+            Assert.AreEqual(_model.Numerator, _domain.Numerator);
+            Assert.AreEqual(_model.Denominator, _domain.Denominator);
+        }
+
+        [Test]
+        public void ShouldMapFractionToFractionModel()
+        {
+            var _model = FractionMapper.Map(_domain);
+
+            Assert.IsNotNull(_model);
+
+            Assert.AreEqual(_domain.Numerator, _model.Numerator);
+            Assert.AreEqual(_domain.Denominator, _model.Denominator);
+        }
+    }
+}
